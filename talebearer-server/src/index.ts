fa1b417fd34969ba707421836c64b5ce4d574e49
rowend36 <rowendduke36@gmail.com>
2023-10-29 21:34:47 +0100

    feat: confused

--- talebearer-server/src/index.ts
+++ talebearer-server/src/index.ts
@@ -1,8 +1,12 @@
+<<<<<<< HEAD
 import engine from "engine.io";
 import { Server } from "socket.io";
 import { Engine } from "engine.io";
 var io = new Server();
 io.at;
+=======
+import { parseDataBody } from 'ably'
+>>>>>>> 5431de0 (feat: start)
 
 // setting up diffsync's DataAdapter
 var diffsync = require("diffsync");
@@ -15,9 +19,9 @@ const server = new engine.Server();
 server.on("connection", (socket) => {
   socket.send("hi");
 });
-
-// …
-httpServer.on("upgrade", (req, socket, head) => {
+<F8><F11>
+<F4// …
+httpServer.on("upgrade", (req, socket, head) =1q> {
   server.handleUpgrade(req, socket, head);
 });
 
@@ -43,6 +47,7 @@ async function handleRequest(request) {
     webSocket: client,
   });
 }
+<<<<<<< HEAD
 export default {
   async fetch(
     request: Request,
@@ -51,4 +56,57 @@ export default {
   ): Promise<Response> {
     return io.se;
   },
+=======
+
+const key = 'YOUR_API_KEY_HERE';
+
+async function handleRequest(request) {
+    try {
+        const body = await request.text();   // Get the body from the response
+        const data = JSON.parse(body);       // Parse body to JavaScript object
+        
+        if (!data) {
+            return jsonResponse(null, 'Request Body cannot be empty!');
+        }
+
+        // Unpack the stringified message we're posting from our HTML game 
+        const incomingPublisedData = parseDataBody(data);
+
+        // Call Ably REST API to publish message
+        await publishToAbly(incomingPublisedData);
+
+        // Return a response from the Cloudflare function
+        // This response is discarded in this example.
+        return new Response ("OK");
+
+    } catch (error) {
+        return new Response(error, { status: 500 });
+    }
+}
+
+
+async function publishToAbly(channel, data) {
+    try {
+        const URL = `https://rest.ably.io/channels/${channel}/messages?key=${key}`
+        
+        await fetch(`${URL}`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ data })
+        });
+
+    } catch (error) {
+        return error
+    }
+}
+
+export default {
+	async fetch(
+		request: Request,
+		env: Env,
+		ctx: ExecutionContext
+	): Promise<Response> {
+		return handleRequest(request);
+	},
+>>>>>>> 5431de0 (feat: start)
 };
